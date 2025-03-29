dd4b20f75e666e604490069b174a9b3f56f5db55
AILalev22 <AILalev22@codingburgas.bg>
2025-03-30 00:42:34 +0200

    Fix PrintEvent loop

--- app/Core/source/list.cpp
+++ app/Core/source/list.cpp
@@ -59,10 +59,13 @@ void ListEvent::PrintEvent(std::string topic) const
 
 	while (condition)
 	{
-		EventData* currEvent = &thead->data;
-		std::cout << "Event name: " << currEvent->name << std::endl;
-		std::cout << "Year: " << currEvent->year << std::endl;
-		std::cout << "Topic: " << currEvent->topic << std::endl;
+		if (topic == thead->data.topic && !topic.empty())
+		{
+			EventData* event = &thead->data;
+			std::cout << "Event name: " << event->name << std::endl;
+			std::cout << "Year: " << event->year << std::endl;
+			std::cout << "Topic: " << event->topic << std::endl;
+		}
 		// other data print later on
 		thead = thead->next;
 	}
