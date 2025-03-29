8a7ca2c4a1a2db7d233d3193f2840c5dbb9ed36f
AILalev22 <AILalev22@codingburgas.bg>
2025-03-29 16:33:46 +0200

    Fix InsertPos function

---
+++ app/Core/source/list.cpp
@@ -0,0 +1,63 @@
+#include "pch.h"
+#include "list.h"
+
+void ListEvent::InsertFront(const EventData& data)
+{
+	List<EventData>::InsertFront(data);
+}
+void ListEvent::InsertBack(const EventData& data)
+{
+	List<EventData>::InsertBack(data);
+}
+void ListEvent::InsertDate(const EventData& data)
+{
+	Node* node = new Node;
+	node->data = data;
+	
+	Node* ppos = head;
+	while (ppos->data.year < data.year)
+		ppos = ppos->next;
+
+	List<EventData>::InsertPos(ppos, node);
+}
+//void ListEvent::InsertDate(const EventData& data)
+//{
+//    Node* ppos = head;
+//    Node* prevPpos = nullptr;
+//    Node* add = new Node;
+//    add->data = data;
+//
+//    if (!ppos) 
+//    {
+//        List<EventData>::InsertFront(data);
+//        return;
+//    }
+//   
+//    while (ppos && ppos->data.year < data.year) 
+//    {
+//        prevPpos = ppos;
+//        ppos = ppos->next;
+//    }
+//
+//    if (!ppos) 
+//    {
+//        List<EventData>::InsertBack(data);
+//    }
+//    else 
+//    {
+//        InsertPos(prevPpos, add);
+//    }
+//}
+void ListEvent::PrintEvent()
+{
+	Node* thead = head;
+	while (thead)
+	{
+		EventData* currEvent = &thead->data;
+		std::cout << "Event name: " << currEvent->name << std::endl;
+		std::cout << "Year: " << currEvent->year << std::endl;
+		std::cout << "Topic: " << currEvent->topic << std::endl;
+		// other data print later on
+		thead = thead->next;
+	}
+}
