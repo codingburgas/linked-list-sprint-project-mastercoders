30f6cb1cebb5bfb51c2e0a672e466b78ca321d0e
AILalev22 <AILalev22@codingburgas.bg>
2025-04-02 17:57:29 +0300

    Add missing Init and Release functions for logic layers

--- app/Presentation/source/presentation.cpp
+++ app/Presentation/source/presentation.cpp
@@ -1,17 +1,27 @@
 #include "pch.h"
 #include "presentation.h"
 #include "access.h"
+#include "business.h"
+#include "core.h"
 namespace Presentation
 {
 	bool Init()
 	{
+		if (!Core::Init())
+			return false;
 		if (!Access::Init())
+			return false;
+		if (!Business::Init())
 			return false;
 		return true;
 	}
 	bool Release()
 	{
 		if (!Access::Release())
+			return false;
+		if (!Access::Release())
+			return false;
+		if (!Business::Release())
 			return false;
 		return true;
 	}
