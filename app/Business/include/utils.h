3b1d7487793d01cb1c899629c52f4efa23bf2856
AILalev22 <AILalev22@codingburgas.bg>
2025-04-03 02:28:46 +0300

    Move utils and options namespaces to business lib

---
+++ app/Business/include/utils.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Utils
+{
+	// clear screen cmd command
+	void Clear();
+	// check if global event list is not init
+	void CheckNotInit();
+	// enter passed event's data
+	void EnterEventData(EventData& event);
+	// loop while non-negative number is entered
+	void EnterNumber(std::string& num);
+	// exit with err msg as quiting
+	void Exit();
+	// show err msg for 0.5 seconds
+	void ErrMsg(const std::string& msg);
+}
